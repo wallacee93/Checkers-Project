434b6afa0bdfc27e0b7290b5a30e70cc4534fd0e
DeShawn Wallace <deshawnwallace@DeShawns-MacBook-Air.local>
2021-09-15 11:51:42 -0400

    RandomSelector

--- src/main/java/Checkers.java
+++ src/main/java/Checkers.java
@@ -3,6 +3,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
+import java.util.Random;
+
 public class Checkers {
 
 
@@ -11,7 +14,7 @@ public class Checkers {
 
     public Checkers(){
 //        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
-        System.setProperty("webdriver.chrome.driver", "/Users/kalebburd/Documents/Programs/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/deshawnwallace/Documents/Programs/chromedriver");
         driver = new ChromeDriver();
         driver.get("https://cardgames.io/checkers/");
         numOfMoves = 0;
@@ -36,11 +39,33 @@ public class Checkers {
     }
 
     public void makeMove(){
-        // Your code here
+
+        Random random = new Random();
+
+//        WebElement s1 = driver.findElement(By.className("s1"));
+//        WebElement s2 = driver.findElement(By.className("s2"));
+//        WebElement s3 = driver.findElement(By.className("s3"));
+//        WebElement s4 = driver.findElement(By.className("s4"));
+//        WebElement s5 = driver.findElement(By.className("s5"));
+//        WebElement s6 = driver.findElement(By.className("s6"));
+//        WebElement s7 = driver.findElement(By.className("s7"));
+//        WebElement s8 = driver.findElement(By.className("s8"));
+//
+//        WebElement s9 = driver.findElement(By.className("s9"));
+//        WebElement s10 = driver.findElement(By.className("s10"));
+//        WebElement s11 = driver.findElement(By.className("s11"));
+//        WebElement s12 = driver.findElement(By.className("s12"));
+
+
+        List<WebElement> startMove = driver.findElements(By.className("active"));
+        int randomStartMove = random.nextInt(startMove.size());
+        startMove.get(randomStartMove).click();
+
+        List<WebElement> makeMove = driver.findElements(By.className("move"));
+        int randomMakeMove = random.nextInt(makeMove.size());
+        makeMove.get(randomMakeMove).click();
+
+
+
     }
-
-
-
-
-
 }
